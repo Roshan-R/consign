7916ae350bd184c46d013a7d5bb0b5debe7501ac
Roshan-R <roshan@cet.ac.in>
2022-12-03 17:56:08 +0530

    added stuff for ashish

--- pages/connection/index.js
+++ pages/connection/index.js
@@ -9,8 +9,16 @@ function Connection() {
   })
   const { disconnect } = useDisconnect()
 
+  // custom helper Class for interfacing with smart-contract
   const helper = new ContractHelper();
-  helper.gett();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+    console.log(e.target[1].value);
+    
+    helper.gett();
+  };
 
   return(
     <>
@@ -22,6 +30,17 @@ function Connection() {
         :
         <button onClick={() => connect()}>Connect Wallet</button>
     }
+
+      <div className="h-screen flex flex-col items-center justify-center">
+        <p className="text-green-700 text-xxl mb-3 ml-1">CREATE WALLET</p>
+        <form onSubmit={handleForm} method="post">
+          <div className="grid gap-6 mb-6 md:grid-rows-2">
+            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Send to" required></input>
+            <input type="number" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Min. approvals" required></input>
+            <input type="submit"></input>
+          </div>
+        </form>
+      </div>
       </>
   )
 
